35a7a51b996c553cc97afd1fac45ec3c38d83b8c
balagangadharanr <balagangadharanr@gmail.com>
2019-10-02 10:39:38 +0530

    fb commit

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -23,6 +23,7 @@ namespace MyAngular8_2.Controllers
                 new Customer { CustomerName = "Bala", CustomerPhone = "9535447447", CustomerEmail = "bala@gmail.com"},
                 new Customer { CustomerName = "Ganga", CustomerPhone = "9535447447", CustomerEmail = "Ganga@gmail.com"},
                 new Customer { CustomerName = "Dharan", CustomerPhone = "9535447447", CustomerEmail = "Dharan@gmail.com"},
+				new Customer { CustomerName = "Vasigaran", CustomerPhone = "9535447447", CustomerEmail = "Vasigaran@gmail.com"},
             };
 
             return customers;
